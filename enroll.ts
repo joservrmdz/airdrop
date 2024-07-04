eb78583e0c05700089d8282e89156f5147e6b99c
José Ramón Rivera <joseramonrivera21@gmail.com>
2024-07-04 07:07:42 -0600

    Final project

---
+++ enroll.ts
@@ -0,0 +1,34 @@
+import { Connection, Keypair, PublicKey } from "@solana/web3.js"
+import { Program, Wallet, Idl, AnchorProvider } from "@coral-xyz/anchor"
+import idl from "./programs/idl.json"
+import wallet from "./dev-wallet.json";
+
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+const connection = new Connection("https://api.devnet.solana.com");
+
+const github = Buffer.from("joservrmdz", "utf8");
+
+const provider = new AnchorProvider(connection, new Wallet(keypair), { commitment: "confirmed" });
+
+const program = new Program(idl as Idl, provider);
+
+const enrollment_seeds = [Buffer.from("prereq"), keypair.publicKey.toBuffer()];
+const [enrollment_key, _bump] = PublicKey.findProgramAddressSync(enrollment_seeds, program.programId);
+
+(async () => {
+    try {
+      const txhash = await program.methods
+        .complete(github)
+        .accounts({
+          signer: keypair.publicKey,
+        })
+        .signers([
+          keypair
+        ]).rpc();
+      console.log(`Success! Check out your TX here:
+  https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+    } catch (e) {
+      console.error(`Oops, something went wrong: ${e}`)
+    }
+  })();
